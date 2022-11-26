14bf4dd267035fc103fece47dd7c106a2b5a32c5
pREDDY <patrickreddy87@gmail.com>
2022-11-25 23:06:46 -0500

    cleanup

--- ChessVue/src/components/Chessboard.vue
+++ ChessVue/src/components/Chessboard.vue
@@ -270,14 +270,14 @@ export default {
       <tbody>
         <tr>
           <th></th>
-          <th>a</th>
-          <th>b</th>
-          <th>c</th>
-          <th>d</th>
-          <th>e</th>
-          <th>f</th>
-          <th>g</th>
-          <th>h</th>
+          <th>A</th>
+          <th>B</th>
+          <th>C</th>
+          <th>D</th>
+          <th>E</th>
+          <th>F</th>
+          <th>G</th>
+          <th>H</th>
         </tr>
         <tr>
           <th>8</th>
