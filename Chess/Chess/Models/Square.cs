c0d2f5dddb163e63d753bb94ff811c4a7f0aeba6
pREDDY <patrickreddy87@gmail.com>
2022-11-25 19:52:58 -0500

    updated square color

--- Chess/Chess/Models/Square.cs
+++ Chess/Chess/Models/Square.cs
@@ -4,6 +4,8 @@
     {
         public int Id { get; set; }
         public string CoordinatePosition { get; set; }
+        public Constants.Color Color { get; set; }
         public int PieceId { get; set; }
+        public Piece Piece { get; set; }
     }
 }
